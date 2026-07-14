f8b39716c643fbe89b4125c7af3d4abcfb9b1acf
masjidalfalahva <masjidalfalahva@gmail.com>
2026-07-14 12:15:26 -0400

    Add prayer database creation

---
+++ includes/database.php
@@ -0,0 +1,53 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * Create prayer times database table
+ */
+function mapt_create_database() {
+
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . 'masjid_prayer_times';
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+
+        prayer_date date NOT NULL,
+
+        fajr_adhan varchar(20),
+        fajr_iqamah varchar(20),
+
+        sunrise varchar(20),
+
+        dhuhr_adhan varchar(20),
+        dhuhr_iqamah varchar(20),
+
+        asr_adhan varchar(20),
+        asr_iqamah varchar(20),
+
+        maghrib_adhan varchar(20),
+        maghrib_iqamah varchar(20),
+
+        isha_adhan varchar(20),
+        isha_iqamah varchar(20),
+
+        ramadan tinyint(1) DEFAULT 0,
+
+        PRIMARY KEY (id),
+
+        UNIQUE KEY prayer_date (prayer_date)
+
+    ) $charset_collate;";
+
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
+    dbDelta($sql);
+
+}
