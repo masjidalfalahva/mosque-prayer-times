8e9b214e46e0189c2bb3d3cd52e0029b43901517
masjidalfalahva <masjidalfalahva@gmail.com>
2026-07-17 11:51:01 -0400

    Create add-prayer.php

---
+++ admin/add-prayer.php
@@ -0,0 +1,15 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function mapt_render_add_prayer_page() {
+	?>
+	<div class="wrap">
+		<h1>Add Prayer Times</h1>
+
+		<p>The prayer entry form will go here.</p>
+	</div>
+	<?php
+}
