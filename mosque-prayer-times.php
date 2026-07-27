f7d9abdf02b258450108e15650390b862e62c679
masjidalfalahva <masjidalfalahva@gmail.com>
2026-07-27 13:10:49 -0400

    Update mosque-prayer-times.php

--- mosque-prayer-times.php
+++ mosque-prayer-times.php
@@ -11,75 +11,73 @@ Text Domain: masjid-prayer-times
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
-// Plugin version
+// Plugin version.
 define( 'MAPT_VERSION', '1.0.0' );
 
-// Plugin path
+// Plugin path.
 define( 'MAPT_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
-// Plugin URL
+// Plugin URL.
 define( 'MAPT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-// Load plugin files
+// Load plugin files.
 require_once MAPT_PLUGIN_DIR . 'includes/database.php';
 require_once MAPT_PLUGIN_DIR . 'includes/prayer-loader.php';
+
 require_once MAPT_PLUGIN_DIR . 'public/shortcode.php';
+
+require_once MAPT_PLUGIN_DIR . 'admin/jummah-schedule.php';
 require_once MAPT_PLUGIN_DIR . 'admin/admin-menu.php';
 require_once MAPT_PLUGIN_DIR . 'admin/add-prayer.php';
 require_once MAPT_PLUGIN_DIR . 'admin/manage-prayers.php';
 require_once MAPT_PLUGIN_DIR . 'admin/import-prayers.php';
+
 require_once MAPT_PLUGIN_DIR . 'includes/docx-importer.php';
 require_once MAPT_PLUGIN_DIR . 'includes/importer-functions.php';
 
 /**
- * Plugin activation
+ * Plugin activation.
  */
 function mapt_activate_plugin() {
 
-     mapt_create_database();
-
+	mapt_create_database();
 }
 
 register_activation_hook(
-    __FILE__,
-    'mapt_activate_plugin'
+	__FILE__,
+	'mapt_activate_plugin'
 );
 
-
 /**
- * Plugin loaded message (temporary test)
+ * Plugin loaded message.
  */
 function mapt_loaded_message() {
 
-    // This confirms the plugin loads correctly.
-
+	// This confirms the plugin loads correctly.
 }
 
 add_action(
-    'plugins_loaded',
-    'mapt_loaded_message'
+	'plugins_loaded',
+	'mapt_loaded_message'
 );
 
 /**
- * Load plugin styles
+ * Load plugin styles.
  */
 function mapt_load_styles() {
 
-    wp_enqueue_style(
-        'mapt-prayer-style',
-        MAPT_PLUGIN_URL . 'public/css/prayer-times.css',
-        array(),
-        MAPT_VERSION
-    );
-
+	wp_enqueue_style(
+		'mapt-prayer-style',
+		MAPT_PLUGIN_URL . 'public/css/prayer-times.css',
+		array(),
+		MAPT_VERSION
+	);
 }
 
 add_action(
-    'wp_enqueue_scripts',
-    'mapt_load_styles'
+	'wp_enqueue_scripts',
+	'mapt_load_styles'
 );
-
-
